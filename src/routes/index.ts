e35e1ef7406a393561945c93235133253ea5c616
tomio111 <tobilokki99@gmail.com>
2023-06-01 05:19:29 -0500

    feat: matchmaking, leaderboard apis

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import authRoute from "./auth.route";
 import userRoute from "./user.route";
 import matchRoute from "./match.route";
+import leaderboardRoute from "./leaderboard.route";
 
 export default function setupRoute(app: any) {
   app.use("/auth", authRoute);
   app.use("/user", userRoute);
   app.use("/match", matchRoute);
+  app.use("/leaderboard", leaderboardRoute);
 }
